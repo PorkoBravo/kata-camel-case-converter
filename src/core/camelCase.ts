e8213e6bdec6329e8d821455c0d879d6c38ac260
Porkobravo <dario.beiro@outlook.com>
2023-05-17 18:55:49 +0200

    camelCaseKata: refactor extract method to capitalize a word

--- src/core/camelCase.ts
+++ src/core/camelCase.ts
@@ -3,9 +3,13 @@ export function convert(words: string): string {
     let wordInCamelCase = "";
 
     words.split(" ").forEach(word => {
-        wordInCamelCase += word.substring(0,1).toUpperCase() + word.substring(1,word.length)
+        wordInCamelCase += capitalizeWord(word)
     })
 
     return wordInCamelCase
     
+}
+
+function capitalizeWord(word: string) {
+    return word.substring(0, 1).toUpperCase() + word.substring(1, word.length);
 }
